015ff0d7a6c12bdf0a70da44ffc4baf61f115456
chakri <chakradhar@paymate.co.in>
2020-09-12 09:26:24 +0530

    Initial Commit

---
+++ Football Stats/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Football Stats
+//
+//  Created by Chakri on 12/09/20.
+//  Copyright © 2020 Mehul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
